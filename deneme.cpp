11808d00a7afc49f35110eccfc262157e3097040
Batu <ozpinarbatuhan3@gmail.com>
2026-07-31 23:39:03 +0300

    Babalasacagiz

---
+++ deneme.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int ilk_sayi;
+int ikinci_sayi;
+int main(){
+    cout << "ilk sayiyi gir:" << endl;
+    cin >> ilk_sayi;
+    cout << "ikinci sayiyi gir:" << endl;
+    cin >> ikinci_sayi;
+    cout << "Sonuc: " << ilk_sayi % ikinci_sayi;
+
+}
